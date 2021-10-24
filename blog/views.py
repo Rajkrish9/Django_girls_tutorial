49ba9a60724fae91d42fcad1b50b8d8156fbea41
mohan <krishnaraj.mohan@gmail.com>
2021-10-24 17:20:19 -0400

    update post list

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render 
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
+
+    #posts = Post.objects.filter 
+
     return render(request, 'blog/post_list.html', {})
 
     #blog/post_list.html
